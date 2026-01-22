67f3905e0e1ea0a7ced0c163ca5f0f48f32c9677
wudixiaoTATA <1275836415@qq.com>
2026-01-22 16:43:16 +0800

    完成第七章卡片练习

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -14,9 +14,9 @@ export default async function LatestInvoices({
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: Uncomment this code in Chapter 7 */}
-
-        {/* <div className="bg-white px-6">
+        
+        {/* 把下面这一行的注释符号 {/* 删掉 */}
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -53,7 +53,9 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div> 
+        {/* 把上面这一行的注释符号 */}
+
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
